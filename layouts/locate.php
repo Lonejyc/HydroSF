d950638497fbec273399ea6083599381ef0bccfb
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-01-16 18:37:01 +0100

    Locate et single post update to correctly work

---
+++ layouts/locate.php
@@ -0,0 +1,27 @@
+<?php 
+    $quote = $content["quote"];
+    $background_img = $content["background_img"];
+    $location_title = $content["location_title"];
+    $location_img = $content["location_img"];
+?>
+
+<article>
+    <img 
+        loading="lazy" 
+        src="<?php echo ($background_img['sizes']['art-img']); ?>"
+        width="<?php echo ($background_img['sizes']['art-img-width']); ?>"
+        height="<?php echo ($background_img['sizes']['art-img-height']); ?>"
+        alt="<?php echo $background_img['alt']; ?>"
+    >
+    <div class="content">
+        <p class="chapo"><?php echo $quote ?></p>
+        <h2 class="h2"><?php echo $location_title ?></h2>
+        <img 
+            loading="lazy" 
+            src="<?php echo ($location_img['sizes']['art-img']); ?>"
+            width="<?php echo ($location_img['sizes']['art-img-width']); ?>"
+            height="<?php echo ($location_img['sizes']['art-img-height']); ?>"
+            alt="<?php echo $location_img['alt']; ?>"
+        >
+    </div>
+</article>
--- single-post.php
+++ single-post.php
@@ -1,16 +1,11 @@
 <?php 
     /* Template Name: Article */
     get_header();
-
-    $art_background_img = get_field('art_background_img');
-    $art_quote = get_field('art_quote');
-    $art_location_title = get_field('art_location_title');
-    $art_location_img = get_field('art_location_img');
 ?>
+<main>
 <?php
 
-
-$flexible_contents = get_field('content');
+$flexible_contents = get_field('head_content', get_the_id());
 
 foreach ($flexible_contents as $content) {
   $template_name = $content['acf_fc_layout'];
@@ -25,29 +20,6 @@ foreach ($flexible_contents as $content) {
 
 }
 ?>
-<main>
-    <section>
-        <article>
-            <img 
-                loading="lazy" 
-                src="<?php echo ($art_background_img['sizes']['art-img']); ?>"
-                width="<?php echo ($art_background_img['sizes']['art-img-width']); ?>"
-                height="<?php echo ($art_background_img['sizes']['art-img-height']); ?>"
-                alt="<?php echo $art_background_img['alt']; ?>"
-            >
-            <div class="content">
-                <p class="chapo"><?php echo $art_quote ?></p>
-                <h2 class="h2"><?php echo $art_location_title ?></h2>
-                <img 
-                    loading="lazy" 
-                    src="<?php echo ($art_location_img['sizes']['art-img']); ?>"
-                    width="<?php echo ($art_location_img['sizes']['art-img-width']); ?>"
-                    height="<?php echo ($art_location_img['sizes']['art-img-height']); ?>"
-                    alt="<?php echo $art_location_img['alt']; ?>"
-                >
-            </div>
-        </article>
-    </section>
 </main>
 <?php
     get_footer();
