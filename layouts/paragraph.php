6d67b6e46152549a7e96a824d1b066c6913b67f8
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-01-16 20:52:06 +0100

    paragraph template create

---
+++ layouts/paragraph.php
@@ -0,0 +1,9 @@
+<?php 
+    $title = $flex_content["title"];
+    $paragraph = $flex_content["paragraph"];
+?>
+
+<article>
+    <h2 class="h2"><?php echo $title; ?></h2>
+    <p class="paragraph"><?php echo $paragraph; ?></p>
+</article>
--- single-post.php
+++ single-post.php
@@ -5,10 +5,27 @@
 <main>
 <?php
 
-$flexible_contents = get_field('head_content', get_the_id());
+$contents = get_field('head_content', get_the_id());
 
-foreach ($flexible_contents as $content) {
+foreach ($contents as $content) {
   $template_name = $content['acf_fc_layout'];
+  $template_file = sprintf('%s/layouts/%s.php', get_template_directory(), $template_name);
+
+  if (file_exists($template_file)) {
+    
+    printf('<section class="layout_%s">', $template_name);
+    include($template_file);
+    printf('</section>');
+  }
+
+}
+?>
+<?php
+
+$flex_contents = get_field('flex_content', get_the_id());
+
+foreach ($flex_contents as $flex_content) {
+  $template_name = $flex_content['acf_fc_layout'];
   $template_file = sprintf('%s/layouts/%s.php', get_template_directory(), $template_name);
 
   if (file_exists($template_file)) {
