58f815fdfca18377f6c9d324e521be1dc7085e08
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-02-23 20:55:20 +0100

    paragraph scss done for pc

--- layouts/paragraph.php
+++ layouts/paragraph.php
@@ -3,7 +3,7 @@
     $paragraph = $flex_content["paragraph"];
 ?>
 
-<article>
+<article class="paragraph-content">
     <h2 class="h2"><?php echo $title; ?></h2>
     <div class="paragraph"><?php echo $paragraph; ?></div>
 </article>
