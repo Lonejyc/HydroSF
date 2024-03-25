fc958dda26b19794dac56b42cb51ca08e018eecf
Jocelyn <128290009+Lonejyc@users.noreply.github.com>
2024-03-25 20:09:24 +0100

    responsive for header and footer fixed

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,13 +2,16 @@ function toggleMenu() {
     const nav = document.querySelector('.container');
     const burger = document.querySelector('.burger');
     const navLink = document.querySelectorAll('.nav-menu-list > li');
+    const body = document.querySelector('body');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('show_nav');
+        body.classList.toggle('overflow-hidden');
     });
     navLink.forEach((link) => {
         link.addEventListener('click', () => {
             nav.classList.remove('show_nav');
+            body.classList.remove('overflow-hidden');
         });
     });
 }
